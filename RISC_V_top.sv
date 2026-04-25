8802c6f21c11b32e4ef9c6497cec13e61b8d8fd7
UnknownPaella <jeffks218@gmail.com>
2026-04-25 15:11:27 +0100

    added RISC V specification docs added high level abstract components

--- RISC_V_top.sv
+++ RISC_V_top.sv
@@ -4,4 +4,12 @@ module RISC_V_top (
     input clk
 );
 
+  registers registers_inst ();
+
+  ALU ALU_inst ();
+
+  memory memory_inst ();
+
+  control control_inst ();
+
 endmodule
---
+++ srcs/control.sv
@@ -0,0 +1,6 @@
+
+
+module control (
+
+);
+endmodule
