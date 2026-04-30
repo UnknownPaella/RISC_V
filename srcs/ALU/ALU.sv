827bdad1f41bd8601905562b9cf1869780e4d074
UnknownPaella <94327166+UnknownPaella@users.noreply.github.com>
2026-04-30 11:08:52 +0100

    instruction decode stuff
    put in logic to extract information from instruction
    
    shifted immediate sign extension logic to decode and not ALU

--- srcs/ALU/ALU.sv
+++ srcs/ALU/ALU.sv
@@ -20,8 +20,7 @@ module ALU #(
 
 );
 
-
-  logic [DATA_WIDTH - 1 : 0] imm_ext = 0;  // sign-extended immediate for arithmetic
+  logic [DATA_WIDTH - 1 : 0] imm_reg = 0;  // sign-extended immediate for arithmetic
   logic [DATA_WIDTH - 1 : 0] rs1_data_reg;  // rs1 hold register
   logic [DATA_WIDTH - 1 : 0] rs2_data_reg;  // rs2 hold register
   logic [DATA_WIDTH - 1 : 0] addr_out_reg;  // adder output register
@@ -39,8 +38,7 @@ module ALU #(
 
   always_ff @(posedge clk) begin : sign_extend_immediate
     if (enable) begin
-      imm_ext[DATA_WIDTH-1 : IMM_WIDTH] <= {(DATA_WIDTH - IMM_WIDTH) {imm[IMM_WIDTH-1]}};
-      imm_ext[IMM_WIDTH-1 : 0] <= imm;
+      imm_reg <= imm;
     end
   end
 
@@ -53,16 +51,16 @@ module ALU #(
 
   always_ff @(posedge clk) begin : blockName
     case (funct3)
-      ADDSUB_FN3, ADDSUBI_FN3: begin // adder output
+      ADDSUB_FN3, ADDSUBI_FN3: begin  // adder output
         rd_data_reg <= adder_out_reg;
       end
-      XOR_FN3, XORI_FN3, OR_FN3, ORI_FN3, AND_FN3, ANDI_FN3: begin // logic unit output
+      XOR_FN3, XORI_FN3, OR_FN3, ORI_FN3, AND_FN3, ANDI_FN3: begin  // logic unit output
         rd_data_reg <= logicComb_out_reg;
       end
-      SLL_FN3, SLLI_FN3, SR_FN3, SRI_FN3: begin // shifter output
+      SLL_FN3, SLLI_FN3, SR_FN3, SRI_FN3: begin  // shifter output
         rd_data_reg <= shifter_out_reg;
       end
-      SLT_FN3, SLTI_FN3, SLTU_FN3, SLTIU_FN3: begin // comparator output
+      SLT_FN3, SLTI_FN3, SLTU_FN3, SLTIU_FN3: begin  // comparator output
         rd_data_reg <= comp_out_reg;
       end
       default: ;
--- srcs/control/decode.sv
+++ srcs/control/decode.sv
@@ -13,11 +13,11 @@ module decode #(
     output [REG_ADDR_WIDTH - 1 : 0] rs2_addr
 );
 
-  localparam bit IMM_11_5_START = 31;  // also start of funct7
-  localparam bit IMM_11_5_END = 25;  // also end of funct7
+  localparam bit IMM_START = 31;  // also start of funct7
+  localparam bit IMM_END = 25;  // also end of funct7
 
   localparam bit RS2_START = 24;
-  localparam bit RS2_END = 20; // also end of imm[11 : 0]
+  localparam bit RS2_END = 20;  // also end of imm[11 : 0]
 
   localparam bit RS1_START = 19;
   localparam bit RS1_END = 15;
@@ -34,45 +34,117 @@ module decode #(
   logic [OPCODE_START : OPCODE_END] inst_opcode;
 
   logic [6 : 0] funct7_reg = 0;
-  logic [19 : 0] imm_reg = 0;
-  logic [4 : 0] rs1_reg = 0;
-  logic [4 : 0] rs2_reg = 0;
-  logic [4 : 0] rsd_reg = 0;
+  logic [31 : 0] imm_reg = 0;
+  logic [4 : 0] rs1_addr_reg = 0;
+  logic [4 : 0] rs2_addr_reg = 0;
+  logic [4 : 0] rd_addr_reg = 0;
   logic [2 : 0] funct3_reg = 0;
+  logic [11 : 0] funct12_reg = 0;
 
 
   always_ff @(posedge clk) begin : inst_decode
     case (inst_opcode)
-      RV32I_REG: begin
+      RV32I_REG: begin  // R-type inst
+        funct7 <= inst[IMM_START : IMM_END];
+        rs2_addr_reg <= inst[RS2_START : RS2_END];
+        rs1_addr_reg <= inst[RS1_START : RS1_END];
+        funct3_reg <= inst[FN3_START : FN3_END];
+        rd_addr_reg <= inst[RD_START : RD_END];
 
+        imm_reg <= 0;
       end
-      RV32I_IMM: begin
-        imm_reg <= inst[IMM_11_5_START : RS2_END];
-        
-      end
-      RV32I_LDR: begin
+      RV32I_IMM: begin  // I-type inst
+        imm_reg[11 : 0] <= inst[IMM_START : RS2_END];
+        rs1_addr_reg <= inst[RS1_START : RS1_END];
+        funct3_reg <= inst[FN3_START : FN3_END];
+        rd_addr_reg <= inst[RD_START : RD_END];
 
-      end
-      RV32I_STR: begin
+        rs2_addr_reg <= 0;
+        funct7 <= 0;
 
+        imm_reg[31 : 12] <= {(31 - 12 + 1){inst[31]}};
       end
-      RV32I_BRH: begin
+      RV32I_LDR: begin  // I-type inst
+        imm_reg[11 : 0] <= inst[IMM_START : RS2_END];
+        rs1_addr_reg <= inst[RS1_START : RS1_END];
+        funct3_reg <= inst[FN3_START : FN3_END];
+        rd_addr_reg <= inst[RD_START : RD_END];
 
+        rs2_addr_reg <= 0;
+        funct7 <= 0;
+
+        imm_reg[31 : 12] <= {(31 - 12 + 1){inst[31]}};
       end
-      RV32I_JAL: begin
+      RV32I_STR: begin  // S-type inst
+        imm_reg[11 : 5] <= inst[IMM_START : IMM_END];
+        rs2_addr_reg <= inst[RS2_START : RS2_END];
+        rs1_addr_reg <= inst[RS1_START : RS1_END];
+        funct3_reg <= inst[FN3_START : FN3_END];
+        imm_reg[4 : 0] <= inst[RD_START : RD_END];
 
+        rd_addr_reg <= 0;
+        funct7 <= 0;
+
+        imm_reg[31 : 12] <= {(31 - 12 + 1){inst[31]}};
       end
-      RV32I_JLR: begin
+      RV32I_BRH: begin  // B-type inst
+        imm_reg[12] <= inst[IMM_START];
+        imm_reg[10 : 5] <= inst[IMM_START-1 : IMM_END];
+        rs2_addr_reg <= inst[RS2_START : RS2_END];
+        rs1_addr_reg <= inst[RS1_START : RS1_END];
+        funct3_reg <= inst[FN3_START : FN3_END];
+        imm_reg[4 : 1] <= inst[RD_START : RD_END+1];
+        imm_reg[11] <= inst[RD_END];
+        imm_reg[0] <= 0;
 
+        rd_addr_reg <= 0;
+        funct7 <= 0;
+
+        imm_reg[31 : 13] <= {(31 - 13 + 1){inst[31]}};
       end
-      RV32I_ENV: begin
+      RV32I_JAL: begin  // J-type inst
+        imm_reg[20] <= inst[IMM_START];
+        imm_reg[10 : 1] <= inst[IMM_START-1 : RS2_END+1];
+        imm_reg[11] <= inst[RS2_END];
+        imm_reg[19 : 12] <= inst[RS1_START : FN3_END];
+        rd_addr_reg <= inst[RD_START : RD_END];
+        imm_reg[0] <= 0;
 
+        funct7 <= 0;
+        rs2_addr_reg <= 0;
+        rs1_addr_reg <= 0;
+        funct3_reg <= 0;
+
+        imm_reg[31 : 21] <= {(31 - 21 + 1){inst[31]}};
       end
-      RV32I_LUI: begin
+      RV32I_JLR: begin  // I-type inst
+        imm_reg[11 : 0] <= inst[IMM_START : RS2_END];
+        rs1_addr_reg <= inst[RS1_START : RS1_END];
+        funct3_reg <= inst[FN3_START : FN3_END];
+        rd_addr_reg <= inst[RD_START : RD_END];
 
+        rs2_addr_reg <= 0;
+        funct7 <= 0;
+
+        imm_reg[31 : 12] <= {(31 - 12 + 1){inst[31]}};
       end
-      RV32I_AUI: begin
+      RV32I_ENV: begin  // I-type inst
+        funct12_reg  <= inst[IMM_START : RS2_END];
+        rs1_addr_reg <= inst[RS1_START : RS1_END];
+        funct3_reg   <= inst[FN3_START : FN3_END];
+        rd_addr_reg  <= inst[RD_START : RD_END];
 
+        imm_reg <= 0;
+      end
+      RV32I_LUI: begin  // U-type inst
+        imm_reg[31 : 12] <= inst[IMM_START : FN3_END];
+        rd_addr_reg <= inst[RD_START : RD_END];
+
+        imm_reg[11 : 0] <= {(11 - 0 + 1){1'b0}};
+      end
+      RV32I_AUI: begin  // U-type inst
+        imm_reg[31 : 12] <= inst[IMM_START : FN3_END];
+        rd_addr_reg <= inst[RD_START : RD_END];
       end
       default: ;
     endcase
